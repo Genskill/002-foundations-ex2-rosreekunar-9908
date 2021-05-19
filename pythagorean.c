f845639149458b43f7342011c13755aedfffff0d
rosreekunar-9908 <84075065+rosreekunar-9908@users.noreply.github.com>
2021-05-19 14:47:52 +0530

    Update pythagorean.c

--- pythagorean.c
+++ pythagorean.c
@@ -6,21 +6,21 @@ int main (void)
   a=get_float("side 1 ");
   b=get_float("side 2 ");
   c=get_float("side 3 ");
-  if (a>b,a>c)
+  if (a>b&&a>c)
   {
     if((a*a)==((b*b)+(c*c)))
       printf ("Yes");
     else
       printf ("No");  
   }
-  else if (b>a,b>c)
+  else if (b>a&&b>c)
   {
-    if((b*b)==((a*a)+(c*c)))
+    if((b*b)==((c*c)+(a*a)))
       printf ("Yes");
     else
       printf ("No");  
   }
-  else if (c>a,c>b)
+  else
   {
     if((c*c)==((a*a)+(b*b)))
       printf ("Yes");
