612c8322df1af239752d81f31bce2fba1bc44fdb
rosreekunar-9908 <84075065+rosreekunar-9908@users.noreply.github.com>
2021-05-19 15:03:19 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -5,36 +5,32 @@ int main (void)
   int ch;
   ch=get_int("Enter type of pattern (1 or 2) ");
   int row;
-  row=get_int("Number of rows ");
   int space;
-  switch (ch)
+  if (ch==1)
   {
-    case 1 : for (int i=row;i>=1;i--)
-             {
-               for (int j=1;j<=i;j++)
-                 printf ("#");
-               printf ("\n");
-             }
-             break;
-             
-    case 2 : space=1;
-             for (int i=row;i>=1;i--)
-             {
-               for (int j=1;j<=i-1;j++)
-               {
-                 printf (" ");
-               }
-               for (int k=1;k<=space;k++)
-               {
-                 printf ("#");
-               } 
-               printf ("\n");
-               space++;  
-             }
-             break;
-             
-    default : printf("Inavalid choice");
-              break;
-                     
-  }           
-}                 
+    row=get_int("Number of rows ");
+    for (int i=row;i>=1;i--)
+    {
+      for (int j=1;j<=i;j++)
+        printf ("#");
+      printf ("\n");
+    }
+   }
+   else if (ch==2)
+   {
+     row=get_int("Number of rows ");
+     space=1;
+     for (int i=row;i>=1;i--)
+     {
+       for (int j=1;j<=i-1;j++)
+         printf (" ");
+       for (int k=1;k<=space;k++)
+         printf ("#");
+       printf ("\n");
+       space++;  
+      }
+    }             
+    else         
+    printf("Inavalid choice");
+}
+                    
